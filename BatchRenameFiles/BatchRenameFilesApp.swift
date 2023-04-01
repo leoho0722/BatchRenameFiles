eed7b711cbd503da202bfb3ac2df90385ca67cfb
leoho0722 <leo160918@gmail.com>
2023-04-02 01:30:37 +0800

    Initial Commit

---
+++ BatchRenameFiles/BatchRenameFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatchRenameFilesApp.swift
+//  BatchRenameFiles
+//
+//  Created by Leo Ho on 2023/4/2.
+//
+
+import SwiftUI
+
+@main
+struct BatchRenameFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
